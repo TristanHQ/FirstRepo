6584ca927e32cd389bc136b96aa82b6f7f576acd
TristanHQ <jiexunpc@163.com>
2018-01-05 16:07:18 +0800

    NewHnt

--- hannuota.py
+++ hannuota.py
@@ -1,19 +1,21 @@
-L = []
+f = open("hnt.txt",'w+')
 def move(n,a,b,c):
     if n == 1:
-        step = (a+str(n)+'-->'+c+str(n))
-        L.append(step)
+        step = (a+str(n)+'-->'+c+str(n)+'\n')
+#        L.append(step)
+        f.write(step)
         return
     else:
         move(n-1,a,c,b)
-        step = (a+str(n)+'-->'+c+str(n))
-        L.append(step)
+        step = (a+str(n)+'-->'+c+str(n)+'\n')
+#        L.append(step)
+        f.write(step)
         move(n-1,b,a,c)
 
-f = open("hnt.txt",'w')
+
 move(5,'A','B','C')
-f.write(str(L))
+#f.write(str(L))
 f = open("hnt.txt",'r')
-s = f.readline()
+s = f.read()
 print(s)
 f.close()
